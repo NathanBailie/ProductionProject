b3829e1dc3bdc244ac561a1759130f2739968e27
Nathan <antnefedov33@gmail.com>
2023-09-01 14:42:12 +0300

    created widget Navbar and AppLink for it

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const { theme, themeToggler } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Link to={'/'} >Main</Link>
-            <Link to={'/about'} >About</Link>
+            <Navbar />
             <button onClick={themeToggler}>Togglt theme</button>
             <AppRouter />
         </div>
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import { classNames } from 'shared/lib/classNames';
+import cls from './Navbar.module.scss';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
+
+interface NavbarProps {
+    className?: string
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [])}>
+            <div className={cls.links}>
+                <AppLink to={'/'} className={cls.mainLink}>Main</AppLink>
+                <AppLink to={'/about'} >About</AppLink>
+            </div>
+        </div>
+    );
+};
