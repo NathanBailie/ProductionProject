d4d6b35eb41f4e3d2978b65bb576b9d29ac681bf
Nathan <antnefedov33@gmail.com>
2023-09-02 14:15:23 +0300

    created Sidebar component and some variables for it

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,6 @@
 import { classNames } from 'shared/lib/classNames';
 import cls from './Navbar.module.scss';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
-
 
 interface NavbarProps {
     className?: string
@@ -12,7 +10,6 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <div className={classNames(cls.Navbar, {}, [])}>
-            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink to={'/'} className={cls.mainLink}>Main</AppLink>
                 <AppLink to={'/about'} >About</AppLink>
