3b70ba9f6324613308b4a4041f47a91e202445a0
Nathan <antnefedov33@gmail.com>
2023-09-13 18:05:37 +0300

    fixed tests for Sidebar, add componentRender for tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Tests for Sidebar component', () => {
     test('Test for rendering', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Test of collapsing of the sidebar', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleButton = screen.getByTestId('sidebar-toggler');
         fireEvent.click(toggleButton);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
