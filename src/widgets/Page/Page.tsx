7f7727f3fcaa85f0b2de88e487e61552376a0317
Nathan <antnefedov33@gmail.com>
2024-07-03 13:42:18 +0300

    added react-virtualized, optimized article list

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -14,6 +14,7 @@ import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEf
 import { useSelector } from 'react-redux';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
+import { PAGE_ID } from 'shared/const/localstorage';
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -54,6 +55,7 @@ export const Page = memo((props: PageProps) => {
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
